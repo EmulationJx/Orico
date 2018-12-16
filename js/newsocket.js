aaf6b91f9784ca68d18074a9bbae335f059aca9a
jinxin <19239663@qq.com>
2018-12-16 23:39:10 +0800

    12-16 current in wirte newsocket web

--- js/newsocket.js
+++ js/newsocket.js
@@ -5,10 +5,22 @@ $(function(){
 	var lpHei = $('.ns_c1_lp').offset().top;
 	var lpdetial = $('.ns_c1_l_detial').offset().top;
 	var c3TitleHei = $('.ns_c3_title').offset().top;
+	var c4TitleHei = $('.ns_c4_title').offset().top;
+	var c4DetailHei = $('.ns_c4_detail').offset().top;
+	var c5DetailHei = $('.ns_c5_detail').offset().top;
+	var c5P = $('.ns_c5_p').offset().top;
+	var c5P1 = $('.ns_c5_p1').offset().top;
+	var c7Title = $('.ns_c7_title').offset().top;
 
 	var selfLpHei = $('.ns_c1_lp').height();
 	var selfLpdetial = $('.ns_c1_l_detial').height();
 	var selfC3TitleHei = $('.ns_c3_title').height();
+	var selfC4TitleHei = $('.ns_c4_title').height();
+	var selfC4DetailHei = $('.ns_c4_detail').height();
+	var selfC5DetailHei = $('.ns_c5_detail').height();
+	var selfC5p = $('.ns_c5_p').height();
+	var selfC5p1 = $('.ns_c5_p1').height();
+	var selfC7Title = $('.ns_c7_title').offset().top;
 
 	$(window).scroll(function(){
 		var scrollHei = $(window).scrollTop();
@@ -18,7 +30,7 @@ $(function(){
 			$('.ns_c1_lp').removeClass('ns_c1_lp_show');
 		}
 
-		if(scrollHei >= lpdetial - $(window).height() && scrollHei <= lpdetial + selfLpdetial ){
+		if(scrollHei >= lpdetial - $(window).height() && scrollHei <= lpdetial + selfLpdetial ){    
 			$('.ns_c1_l_detial').addClass('ns_c1_l_detial_show');
 		}else{
 			$('.ns_c1_l_detial').removeClass('ns_c1_l_detial_show');
@@ -28,6 +40,42 @@ $(function(){
 			$('.ns_c3_title').addClass('ns_c3_title_show');
 		}else{
 			$('.ns_c3_title').removeClass('ns_c3_title_show');
+		}
+
+		if(scrollHei >= c4TitleHei - $(window).height() && scrollHei <= c4TitleHei + selfC4TitleHei ){
+			$('.ns_c4_title').addClass('ns_c4_title_show');
+		}else{
+			$('.ns_c4_title').removeClass('ns_c4_title_show');
+		}
+
+		if(scrollHei >= c4DetailHei - $(window).height() && scrollHei <= c4DetailHei + selfC4DetailHei ){
+			$('.ns_c4_detail').addClass('ns_c4_detail_show');
+		}else{
+			$('.ns_c4_detail').removeClass('ns_c4_detail_show ');
+		}
+
+		if(scrollHei >= c5DetailHei - $(window).height() && scrollHei <= c5DetailHei + selfC5DetailHei ){
+			$('.ns_c5_detail').addClass('ns_c5_detail_show');
+		}else{
+			$('.ns_c5_detail').removeClass('ns_c5_detail_show');
+		}
+
+		if(scrollHei >= c5P - $(window).height() && scrollHei <= c5P + selfC5p ){
+			$('.ns_c5_p').addClass('ns_c5_p_show');
+		}else{
+			$('.ns_c5_p').removeClass('ns_c5_p_show');
+		}
+
+		if(scrollHei >= c5P1 - $(window).height() && scrollHei <= c5P1 + selfC5p1 ){
+			$('.ns_c5_p1').addClass('ns_c5_p1_show');
+		}else{
+			$('.ns_c5_p1').removeClass('ns_c5_p1_show');
+		}
+
+		if(scrollHei >= c7Title - $(window).height() && scrollHei <= c7Title + selfC7Title ){
+			$('.ns_c7_title').addClass('ns_c7_title_show');
+		}else{
+			$('.ns_c7_title').removeClass('ns_c7_title_show');
 		}
 	})
 })
